4da041191ef8ec905e7f79e743c9377c4af7c5a4
abdarrahman abouzaid <abdarrahman.abouzaid@gmail.com>
2020-12-09 02:30:40 +0200

    reset headers

--- sendEmail.php
+++ sendEmail.php
@@ -53,9 +53,9 @@ if($_POST) {
 
 	// Email Headers
 	$headers = "MIME-Version: 1.0\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\n";
 	$headers .= 'From: ' . $from . '\n';
-	$headers .= "Reply-To: " . $email . '\n';
+	$headers .= 'Reply-To: '. $email . '\n';
 
    	if (empty($errors)) {
 		try {
